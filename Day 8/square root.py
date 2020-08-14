29cdb956897cd41ff1ee08bd76af84872ffd875f
Anirudh Singh Chauhan <48491447+Anirudh-Chauhan@users.noreply.github.com>
2020-08-15 00:06:30 +0530

    Add files via upload

---
+++ Day 8/square root.py
@@ -0,0 +1,31 @@
+#Given an integer x. The task is to find the square root of x. If x is not a perfect square, then return floor(vx).
+
+#SOLUTION
+
+
+def floorSqrt(x): 
+    return math.floor(math.sqrt(x))
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            x=int(input())
+            
+            print(floorSqrt(x))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
